242c5f66057916b38b3fa31d9dc7759de264f46a
HOANG TRAN <hoangtran0192@gmail.com>
2016-06-02 21:44:50 +0700

    no message

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-#Lập ra movie_list
-class Movie:
-    def __init__(self, title, img):
-        self.title = title
-        self.img = img
+#import từ thư viện Mongo DB
+import mongoengine
+from mongoengine import Document, StringField
+host = "ds011893.mlab.com"
+port = 11893
+db_name = "c4e_rss"
+user_name = "c4e"
+password = "codethechange"
+mongoengine.connect(db_name, host=host, port=port, username=user_name, password=password)
 
-movie1 = Movie("Tom & Jerry", "https://upload.wikimedia.org/wikipedia/en/5/5f/TomandJerryTitleCardc.jpg")
-movie2 = Movie("Harry Potter", "http://static.independent.co.uk/s3fs-public/styles/article_large/public/thumbnails/image/2013/09/12/17/potter.jpg")
-movie_list = [
-    movie1,
-    movie2,
-    Movie("50 Shades of Grey", "http://cdn04.cdn.justjared.com/wp-content/uploads/headlines/2015/04/fifty-shades-of-grey-writer.jpg")
-    ]
+class Movie(Document): #để báo cho Python là mình lấy dữ liệu từ Document của Mongo DB
+    title = StringField() #Mongo sẽ tự tìm những field có trùng tên ở trên DB và mục này của Python
+    img = StringField()
+
+# #Để update dữ liệu lên Mongo DB:
+# m = Movie(
+#     title = "Tom & Jerry",
+#     img = "https://upload.wikimedia.org/wikipedia/en/5/5f/TomandJerryTitleCardc.jpg"
+# )
+# m.save()
+
+# #Lập ra movie_list
+# class Movie:
+#     def __init__(self, title, img):
+#         self.title = title
+#         self.img = img
+#
+# movie1 = Movie("Tom & Jerry", "https://upload.wikimedia.org/wikipedia/en/5/5f/TomandJerryTitleCardc.jpg")
+# movie2 = Movie("Harry Potter", "http://static.independent.co.uk/s3fs-public/styles/article_large/public/thumbnails/image/2013/09/12/17/potter.jpg")
+# movie_list = [
+#     movie1,
+#     movie2,
+#     Movie("50 Shades of Grey", "http://cdn04.cdn.justjared.com/wp-content/uploads/headlines/2015/04/fifty-shades-of-grey-writer.jpg")
+#     ]
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", m_list = Movie.objects)
 
 @app.route('/<name>')
 def hello(name):
